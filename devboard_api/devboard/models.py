cef375915500f9f82d3fc023c18e09d0e44249b7
nmar30 <nmarcisso@live.com>
2021-06-24 13:04:01 -0400

    Added Models

--- devboard_api/devboard/models.py
+++ devboard_api/devboard/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='owner')
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='members')
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    due_date = models.DateField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class TaskNote(models.Model):
+    task = models.ForeignKey('Task', on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    resources = models.CharField(max_length=200)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
