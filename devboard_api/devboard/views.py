4f51933b7279b1fa36b00b7b508da3589d1d7464
nmar30 <nmarcisso@live.com>
2021-06-24 14:12:03 -0400

    Added Urls.py and Updated Views.py

--- devboard_api/devboard/views.py
+++ devboard_api/devboard/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Project, Task, TaskNote
+from .serializers import ProjectSerializer, TaskSerializer, TaskNoteSerializer
+from rest_framework.views import APIView
+from rest_framework.views import Response
+from rest_framework import status
 
 # Create your views here.
+class ProjectList(APIView):
+    def get(self, request):
+        project = Project.objects.all()
+        serializer = ProjectSerializer(project, many=True)
+        return Response(serializer.data)
