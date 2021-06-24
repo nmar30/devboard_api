aeefcd7edf5f7b63d582598b7a37682142f6a5af
nmar30 <nmarcisso@live.com>
2021-06-24 17:03:01 -0400

    Added Authentication and Updated Views.py

--- devboard_api/devboard/urls.py
+++ devboard_api/devboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('projects/', views.ProjectList.as_view()),
+    path('projects/<int:project_id>', views.ProjectDetails.as_view())
 ]
--- devboard_api/devboard/views.py
+++ devboard_api/devboard/views.py
@@ -4,10 +4,49 @@ from .serializers import ProjectSerializer, TaskSerializer, TaskNoteSerializer
 from rest_framework.views import APIView
 from rest_framework.views import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class ProjectList(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request):
         project = Project.objects.all()
         serializer = ProjectSerializer(project, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProjectDetails(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, project_id):
+        try:
+            return Project.objects.get(pk=project_id)
+        except Project.DoesNotExist:
+            raise Http404
+
+    def get(self, request, project_id):
+        project = self.get_object(project_id)
+        serializer = ProjectSerializer(project)
+        return Response(serializer.data)
+
+    def put(self, request, project_id):
+        project = self.get_object(project_id)
+        serializer = ProjectSerializer(project, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, project_id):
+        project = self.get_object(project_id)
+        serializer = ProjectSerializer(project)
+        project.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
