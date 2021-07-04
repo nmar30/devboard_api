f8ce553308723669f438c23181efa9fdbc730c13
nmar30 <nmarcisso@live.com>
2021-07-04 18:11:05 -0400

    Bug Fixes

--- devboard_api/devboard/urls.py
+++ devboard_api/devboard/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('projects/', views.ProjectList.as_view()),
     path('projects/<int:project_id>/', views.ProjectDetails.as_view()),
     path('projects/<int:project_id>/tasks/', views.TaskList.as_view()),
-    path('projects/<int:project_id>/tasks/<int:task_id>', views.TaskDetails.as_view()),
+    path('projects/<int:project_id>/tasks/<int:task_id>/', views.TaskDetails.as_view()),
     path('projects/<int:project_id>/tasks/<int:task_id>/notes/', views.TaskNoteList.as_view()),
-    path('projects/<int:project_id>/tasks/<int:task_id>/notes/<int:note_id>', views.TaskNoteDetails.as_view()),
+    path('projects/<int:project_id>/tasks/<int:task_id>/notes/<int:note_id>/', views.TaskNoteDetails.as_view()),
     path('users/', views.UserDetails.as_view()),
 ]
